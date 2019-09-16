e4b40550c5f9e40db12e3f96f5bf019351858c2c
Pierre Bedell <pierre.bedell@gmail.com>
2019-09-16 18:40:03 +0200

    distribute

---
+++ Transformations/Distribute.lua
@@ -0,0 +1,21 @@
+setColor(200, 230, 135)
+
+shape1 = input('shape1', 'SHAPE')
+shape2 = input('shape2', 'SHAPE')
+density = input('density', 'SCALAR', 0.1, 0.0, 1.0, true)
+orient = input('follow normals', 'BOOLEAN', true)
+
+s = distribute(shape1, density)
+centroids = {}
+
+if orient then
+  for i = 1,#s,1 do
+    centroids[i] = translate(s[i][1]) * frame(s[i][2]) * union{ shape2, mirror(Z) * shape2 }
+  end
+else
+  for i = 1,#s,1 do
+    centroids[i] = translate(s[i][1]) * shape2
+  end
+end
+
+output('shape', 'SHAPE', union(centroids))
