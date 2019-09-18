80db400576dc37a34b03541255a0e4f0f73d6aec
Pierre Bedell <pierre.bedell@gmail.com>
2019-09-18 15:39:09 +0200

    3d model node quick hack to prevent errors from icesl

--- Shapes/3D Model.lua
+++ Shapes/3D Model.lua
@@ -1,5 +1,7 @@
 setColor(102, 187, 106)
 
-path = data('path', 'PATH', '', false, 'Object (*.stl, *.obj)\0*.obj;*.stl\0All (*.*)\0*.*\0'  )
+path = data('path', 'PATH', '.', false, 'Object (*.stl, *.obj)\0*.obj;*.stl\0All (*.*)\0*.*\0'  )
+
 shape = load(path)
+
 output('shape', 'SHAPE', shape)
