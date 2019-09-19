a4dc41a7afce8af8ca17a06f4b461bdce169ac09
Pierre Bedell <pierre.bedell@gmail.com>
2019-09-19 18:27:49 +0200

    3d model filter fix

--- Shapes/3D Model.lua
+++ Shapes/3D Model.lua
@@ -1,6 +1,6 @@
 setColor(102, 187, 106)
 
-path = data('path', 'PATH', '.', false, 'Object (*.stl, *.obj)\0*.obj;*.stl\0All (*.*)\0*.*\0'  )
+path = data('path', 'PATH', '.', false, {'*.stl', '*.obj'})
 
 shape = load(path)
 
